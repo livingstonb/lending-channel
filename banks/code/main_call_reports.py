52f157583826436a362cb0b5f8dfcffcdae8505e
Brian Livingston <ak.brian09@gmail.com>
2024-07-27 10:30:13 -0500

    deletion of old file

--- banks/code/main_call_reports.py
+++ banks/code/main_call_reports.py
@@ -11,7 +11,7 @@ import call_report_fns
 import pandas as pd
 import numpy as np
 
-def start(bhcid):
+def start(bhcids):
 	dirs = aux.ProjectDirs()
 	
 	call_table = call_report_fns.get_call(dirs)
@@ -21,16 +21,35 @@ def start(bhcid):
 	
 	
 	recursion = call_report_fns.CallRecursion(call_table, links_table)
-	results = recursion.search(bhcid)
+	
+	results = dict()
+	for bhcid in bhcids:
+		results[bhcid] = recursion.search(bhcid)
+		
 	return (call_table, results)
 
 if __name__ == "__main__":
 	chase = 1039502
-	(call_table, rssdids) = start(chase)
-	idx = list(rssdids)
-	data = np.ones(len(idx))
+	svb = 1031449
+	bhcids = [chase, svb]
 	
-	ser = pd.Series(data, name='matched_rssdid', index=idx)
-	matches = call_table.merge(ser, left_index=True, right_index=True)
-	insured_deposits = matches['RCONF045'] + matches['RCONF049']
-	# convert to double
+	(call_table, rssdids) = start(bhcids)
+	
+	df = pd.DataFrame()
+	for bhcid in bhcids:
+		idx = list(rssdids[bhcid])
+		data = np.ones(len(idx), dtype=np.int32) * bhcid
+	
+		ser = pd.Series(data, name='bhc_rssdid', index=idx)
+		matches = call_table.merge(ser, left_index=True, right_index=True)
+		insured_deposits = matches['RCONF045'] + matches['RCONF049']
+		df = pd.concat([df, matches])
+		
+	variables = {
+		'RCONF045': 'ins_dep_retir',
+		'RCONF049': 'ins_dep_nonretir',
+			  }
+	df.rename(columns=variables, inplace=True)
+	df.index.name = 'rssdid'
+	 
+	
