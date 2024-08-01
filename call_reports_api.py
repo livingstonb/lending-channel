aa5b3da456d4a98d033aab380f3e94d4ca9d37d5
Brian Livingston <ak.brian09@gmail.com>
2024-07-31 20:11:18 -0500

    fixes

--- call_reports_api.py
+++ call_reports_api.py
@@ -8,16 +8,32 @@ def request_call_reports():
     return conn
 
 
-def query(conn, vars):
+def query(conn, vtab, vars):
+    tables = ['wrds_call_rcon_1', 'wrds_call_rcon_2',
+              'wrds_call_rcfd_1', 'wrds_call_rcfd_2']
+    for i, tab in enumerate(tables):
+        v_this_table = [v for v in vtab[tab] if v in vars]
+        if len(v_this_table) == 0:
+            continue
 
-    apple = conn.raw_sql(
-        """select rssd9001, rssd9999, rssd9017
-                %s
-            from bank.wrds_call_rcon_1
-            where rssd9001 = 37
-            and rssd9999 > '20210101 00:00:00'"""%vars,
+        if i == 0:
+            qt = query_one_table(conn, tab, v_this_table)
+        else:
+            xt = query_one_table(conn, tab, v_this_table)
+            icols = ['rssd9001', 'rssd9999']
+            qt = qt.merge(xt, how='outer', on=icols)
+    return qt
+
+
+def query_one_table(conn, table, vars):
+    vstr = ', '.join(vars)
+    df = conn.raw_sql(
+        """select rssd9001, rssd9999, rssd9017, %s
+            from bank.%s
+            where rssd9001 < 10000
+                and rssd9999 = '20210630 00:00:00'"""%(vstr, table),
                          date_cols=['rssd9999'])
-    return apple
+    return df
 
 
 def variables():
@@ -26,9 +42,21 @@ def variables():
     'rcona557': 'rcon_famsec_1y3y',
     'rcona558': 'rcon_famsec_3y5y',
     'rcona559': 'rcon_famsec_5y15y',
-    'rcona560': 'rcon_famsec_ge15y'}
-    vstr = ', '.join(list(vars.keys()))
-    return vars, vstr
+    'rcona560': 'rcon_famsec_ge15y',
+    'rcon2200': 'rcon_deposits',
+    'rcon2170': 'rcon_assets',
+    'rconb993': 'rcon_repo_liab_ff',
+    'rconb995': 'rcon_repo_liab_oth',
+    'rcon3190': 'rcon_oth_borr_money',
+    'rcon3200': 'rcon_sub_debt',
+    'rcon2213': 'rcon_liab_fbk_trans',
+    'rcon2236': 'rcon_liab_fbk_ntrans',
+    'rcon2216': 'rcon_liab_foff_trans',
+    'rcon2377': 'rcon_liab_foff_ntrans',
+    'rcfd2200': 'rcfd_deposits',
+    'rcfd2170': 'rcfd_assets',
+    }
+    return vars
 
 if __name__ == "__main__":
     conn = request_call_reports()
@@ -39,6 +67,6 @@ if __name__ == "__main__":
     vtab = callvars.variables_table
 
     # Select variables
-    vars, vstr = variables()
-    q = query(conn, vstr)
+    vars = variables()
+    q = query(conn, vtab, vars)
     q.rename(columns=vars, inplace=True)
