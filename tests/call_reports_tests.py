1a0f78d6025226a66ef14dfda088ab154a00edee
Brian Livingston <ak.brian09@gmail.com>
2024-12-28 14:11:52 -0900

    file deletions

--- tests/call_reports_tests.py
+++
@@ -1,7 +0,0 @@
-from code.mod_bank import call_reports
-
-
-def assign_topid_up(df):
-    date = 20221231
-    attr_files = ['data/NIC_attributes_closed.csv', 'data/NIC_attributes_active.csv']
-    bhcids = call_reports.assign_topid_up(df, 'data/NIC_relationships.csv', attr_files, date)
